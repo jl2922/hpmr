2ddb2bda6302db440d60d9793378113a4747ede3
Junhao Li <streaver91@gmail.com>
2018-01-25 16:17:24 -0500

    partly working

---
+++ src/reducer.h
@@ -0,0 +1,27 @@
+#ifndef HPMR_REDUCER_H
+#define HPMR_REDUCER_H
+#include <functional>
+
+namespace hpmr {
+
+template <class T>
+class Reducer {
+ public:
+  static void keep(T&, const T&) {}
+
+  static void overwrite(T& t1, const T& t2) { t1 = t2; }
+
+  static void sum(T& t1, const T& t2) { t1 += t2; }
+
+  static void min(T& t1, const T& t2) {
+    if (t1 < t2) t1 = t2;
+  }
+
+  static void max(T& t1, const T& t2) {
+    if (t1 > t2) t1 = t2;
+  }
+};
+
+}  // namespace hpmr
+
+#endif
